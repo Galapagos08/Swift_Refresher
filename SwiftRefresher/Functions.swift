b5c102d0c6102fb42386050c5eb4a649881def90
Dan Esrey <danesrey@Dans-MacBook-Air.local>
2016-09-24 18:51:31 -0400

    initial commit

---
+++ SwiftRefresher/Functions.swift
@@ -0,0 +1,42 @@
+//
+//  Functions.swift
+//  SwiftRefresher
+//
+//  Created by Dan Esrey on 2016/24/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+import Foundation
+
+func awesomeFunction (array: [Int])-> Int {
+    var product = 1
+    for number in array {
+        product = product * number
+    }
+    return product
+}
+
+func enhanceArray (array: [Int])-> [Int]{
+    let n = array.count
+    var enhancedArray = [Int]()
+    var enhancedValue: Int = 0
+    
+    for index in 0...(n-1) {
+        enhancedValue = awesomeFunction(array: array)/array[index]
+        enhancedArray.append(enhancedValue)
+    }
+    return enhancedArray
+}
+
+func dedupeSortedArray (arrayWithDupes: [Int])->([Int], Int) {
+    var result = [Int]()
+    let c = arrayWithDupes.count
+    
+    for index in 0...(c-1) {
+        if !result.contains(arrayWithDupes[index]) {
+            result.append(arrayWithDupes[index])
+        }
+    }
+    return (result, result.count)
+}
+
---
+++ SwiftRefresher/SwiftRefresher.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRefresher.h
+//  SwiftRefresher
+//
+//  Created by Dan Esrey on 2016/24/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftRefresher.
+FOUNDATION_EXPORT double SwiftRefresherVersionNumber;
+
+//! Project version string for SwiftRefresher.
+FOUNDATION_EXPORT const unsigned char SwiftRefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRefresher/PublicHeader.h>
+
+
